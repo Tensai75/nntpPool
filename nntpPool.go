1faaa36566305494f760077379bf81af37fb8eab
Tensai <tensai@gmx.net>
2024-05-19 11:30:11 +0200

    Update nntpPool.go

--- nntpPool.go
+++ nntpPool.go
@@ -6,37 +6,39 @@ import (
 	"errors"
 	"fmt"
 	"sync"
-	"sync/atomic"
 	"time"
 
 	"github.com/Tensai75/nntp"
 )
 
 var (
-	WarnChan = make(chan error, 10)
-	LogChan  = make(chan string, 10)
+	WarnChan  = make(chan error, 10)
+	LogChan   = make(chan string, 10)
+	DebugChan = make(chan string, 10)
 )
 
 var (
 	errMaxConnsShouldBePositive = errors.New("max conns should be greater than 0")
-	errMinIsGreaterThanMax      = errors.New("min conns should be lower than or equal to max conns")
-	errPoolAlreadyClosed        = errors.New("connection pool already closed")
-	errNoConnectionsAvailable   = errors.New("no connections available")
-	errConnIsNil                = errors.New("the connection is nil")
+	errConsIsGreaterThanMax     = errors.New("initial amount of connections should be lower than or equal to max conns")
+	errPoolWasClosed            = errors.New("connection pool was closed")
 )
 
 type ConnectionPool interface {
 	// Returns number of used and total opened connections.
 	Conns() (uint32, uint32)
+	// Returns number of max opened connections.
+	MaxConns() uint32
 	// Retrieves connection from pool if it exists or opens new connection.
 	Get(ctx context.Context) (*NNTPConn, error)
 	// Returns connection to pool.
-	Put(conn *NNTPConn) error
+	Put(conn *NNTPConn)
 	// Closes all connections and pool.
-	Close() error
+	Close()
 }
 
 type Config struct {
+	// Name for the usenet server
+	Name string
 	// Usenet server host name or IP address
 	Host string
 	// Usenet server port number
@@ -49,20 +51,16 @@ type Config struct {
 	User string
 	// Password to connect to the usenet server
 	Pass string
-	// Number of retries per connection upon connection error
-	ConnRetries uint32
-	// Time to wait in seconds before trying to re-connect
-	ConnWaitTime time.Duration
-	// Min number of connections that will be opened during New() function.
-	MinConns uint32
 	// Max number of opened connections.
 	MaxConns uint32
+	// Time to wait in seconds before trying to re-connect
+	ConnWaitTime time.Duration
 	// Duartion after idle connections will be closed
 	IdleTimeout time.Duration
-	// Number of cumulative connection errors after which the pool fails if no connection could be established at all
+	// Number of max "too many connections" errors after which MaxConns is automatically reduced (0 = disabled)
+	MaxTooManyConnsErrors uint32
+	// Number of max consecutive connection errors after which the pool fails if no connection could be established at all (0 = disabled)
 	MaxConnErrors uint32
-	// Number of max "too many connections" errors after which MaxConns is automatically reduced (0 = disabled).
-	// AutoReduceMaxErrors uint32
 }
 
 type NNTPConn struct {
@@ -75,108 +73,110 @@ type connectionPool struct {
 	connsChan   chan NNTPConn
 	addConnChan chan connRequest
 
-	host          string
-	port          uint32
-	ssl           bool
-	skipSslCheck  bool
-	user          string
-	pass          string
-	connRetries   uint32
-	connWaitTime  time.Duration
-	maxRetryErros uint32
-	minConns      uint32
-	maxConns      uint32
-	idleTimeout   time.Duration
+	name                  string
+	host                  string
+	port                  uint32
+	ssl                   bool
+	skipSslCheck          bool
+	user                  string
+	pass                  string
+	maxConns              uint32
+	connWaitTime          time.Duration
+	idleTimeout           time.Duration
+	maxTooManyConnsErrors uint32
+	maxConnErrors         uint32
 
-	conns        atomic.Int32
-	connAttempts atomic.Int32
-	retryErrors  atomic.Int32
-	lastConnTime time.Time
-	closed       bool
-	fatalError   error
-	serverLimit  uint32
-	isRetry      bool
+	conns              uint32
+	connAttempts       uint32
+	lastConnTime       time.Time
+	closed             bool
+	fatalError         error
+	serverLimit        uint32
+	tooManyConnsErrors uint32
+	connErrors         uint32
+	startupWG          sync.WaitGroup
+	created            bool
+	maxConnsUsed       uint32
 }
 
-type connRequest struct {
-	lastConnTime time.Time
-	retryNo      uint32
-	lastErr      error
-}
+type connRequest struct{}
 
 // Opens new connection pool.
-func New(cfg *Config) (ConnectionPool, error) {
+func New(cfg *Config, initialConns uint32) (ConnectionPool, error) {
 	switch {
-	case cfg.MaxConns == 0:
+	case cfg.MaxConns <= 0:
 		return nil, errMaxConnsShouldBePositive
-	case cfg.MinConns > cfg.MaxConns:
-		return nil, errMinIsGreaterThanMax
+	case initialConns > cfg.MaxConns:
+		return nil, errConsIsGreaterThanMax
 	}
 
 	pool := &connectionPool{
 		connsMutex:  sync.RWMutex{},
 		connsChan:   make(chan NNTPConn, cfg.MaxConns),
-		addConnChan: make(chan connRequest, 1),
+		addConnChan: make(chan connRequest, cfg.MaxConns),
 
-		host:         cfg.Host,
-		port:         cfg.Port,
-		ssl:          cfg.SSL,
-		skipSslCheck: cfg.SkipSSLCheck,
-		user:         cfg.User,
-		pass:         cfg.Pass,
-		connRetries:  cfg.ConnRetries,
-		connWaitTime: cfg.ConnWaitTime,
-		maxConns:     cfg.MaxConns,
-		minConns:     cfg.MinConns,
-		idleTimeout:  cfg.IdleTimeout,
+		name:                  cfg.Name,
+		host:                  cfg.Host,
+		port:                  cfg.Port,
+		ssl:                   cfg.SSL,
+		skipSslCheck:          cfg.SkipSSLCheck,
+		user:                  cfg.User,
+		pass:                  cfg.Pass,
+		maxConns:              cfg.MaxConns,
+		connWaitTime:          cfg.ConnWaitTime,
+		idleTimeout:           cfg.IdleTimeout,
+		maxTooManyConnsErrors: cfg.MaxTooManyConnsErrors,
+		maxConnErrors:         cfg.MaxConnErrors,
 
 		closed:       false,
 		lastConnTime: time.Now(),
 		fatalError:   nil,
-		serverLimit:  0,
-		isRetry:      false,
+		serverLimit:  cfg.MaxConns,
 	}
 
 	go pool.addConnHandler()
 
-	for i := uint32(0); i < pool.minConns; i++ {
+	for i := uint32(0); i < initialConns; i++ {
+		pool.startupWG.Add(1)
 		pool.addConn()
 	}
-
-	for {
-		if uint32(pool.conns.Load()) >= pool.minConns {
-			select {
-			case LogChan <- fmt.Sprintf("pool created successfully (%v of %v connections available)", pool.conns.Load(), pool.maxConns):
-			default:
-			}
-			return pool, nil
-		}
-		if uint32(pool.retryErrors.Load()) > pool.maxRetryErros {
-			if pool.conns.Load() > 0 {
-				select {
-				case LogChan <- fmt.Sprintf("pool created with errors (%v of %v connections available)", pool.conns.Load(), pool.maxConns):
-				default:
-				}
-				return pool, nil
-			}
-		}
+	pool.startupWG.Wait()
+	if pool.fatalError != nil {
+		pool.Close()
+		return nil, pool.fatalError
 	}
+	pool.created = true
+	if initialConns > 0 && pool.conns < initialConns {
+		pool.log(fmt.Sprintf("pool created with errors (%v of %v requested connections available)", pool.conns, initialConns))
+	} else {
+		pool.log("pool created successfully")
+	}
+	return pool, nil
 }
 
 // Retrieves connection from the pool if it exists or opens new connection.
 func (cp *connectionPool) Get(ctx context.Context) (*NNTPConn, error) {
+	defer func() {
+		cp.connsMutex.Lock()
+		defer cp.connsMutex.Unlock()
+		usedConns := cp.conns - uint32(len(cp.connsChan))
+		if usedConns > cp.maxConnsUsed {
+			cp.maxConnsUsed = usedConns
+		}
+	}()
 	for {
 		if cp.fatalError != nil {
 			cp.Close()
-			return nil, errNoConnectionsAvailable
+			return nil, cp.fatalError
 		}
 		select {
 		case <-ctx.Done():
+			cp.Close()
 			return nil, ctx.Err()
 
 		case conn, ok := <-cp.connsChan:
 			if !ok {
-				return nil, errPoolAlreadyClosed
+				return nil, errPoolWasClosed
 			}
 			if !cp.checkConnIsHealthy(conn) {
 				continue
@@ -184,10 +184,14 @@ func (cp *connectionPool) Get(ctx context.Context) (*NNTPConn, error) {
 			return &conn, nil
 
 		default:
-			go cp.addConn()
+			cp.connsMutex.Lock()
+			if cp.connAttempts < cp.serverLimit {
+				go cp.addConn()
+			}
+			cp.connsMutex.Unlock()
 			conn, ok := <-cp.connsChan
 			if !ok {
-				return nil, errPoolAlreadyClosed
+				return nil, errPoolWasClosed
 			}
 			if !cp.checkConnIsHealthy(conn) {
 				continue
@@ -198,45 +202,65 @@ func (cp *connectionPool) Get(ctx context.Context) (*NNTPConn, error) {
 }
 
 // Returns connection to the pool.
-func (cp *connectionPool) Put(conn *NNTPConn) error {
-	select {
-	case cp.connsChan <- NNTPConn{Conn: conn.Conn, timestamp: time.Now()}:
-		return nil
-	default:
+func (cp *connectionPool) Put(conn *NNTPConn) {
+	cp.connsMutex.Lock()
+	defer cp.connsMutex.Unlock()
+	if !cp.closed {
 		select {
-		case LogChan <- "closing connection because channel is full":
+		case cp.connsChan <- NNTPConn{Conn: conn.Conn, timestamp: time.Now()}:
+			return
 		default:
+			cp.debug("closing returned connection because pool is full")
 		}
-		return cp.closeConn(conn)
+	} else {
+		cp.debug("closing returned connection because pool is closed")
 	}
+	cp.closeConn(conn)
 }
 
 // Closes all connections and pool.
-func (cp *connectionPool) Close() error {
+func (cp *connectionPool) Close() {
+	cp.debug("closing pool")
 	cp.connsMutex.Lock()
 	defer cp.connsMutex.Unlock()
 	if cp.closed {
-		return nil
+		cp.debug("pool is already closed")
+		return
 	}
 	cp.closed = true
 
-	if cp.connsChan == nil {
-		return nil
+	if cp.connsChan != nil {
+		cp.debug("closing connection channel")
+		close(cp.connsChan)
+		cp.debug("closing open connections")
+		for conn := range cp.connsChan {
+			go conn.close()
+		}
 	}
-	close(cp.connsChan)
-
-	var err error
-	for conn := range cp.connsChan {
-		err = conn.close()
+	if cp.addConnChan != nil {
+		cp.debug("closing add connection channel")
+		close(cp.addConnChan)
 	}
-
-	return err
+	cp.debug("pool closed")
 }
 
 // Returns number of opened connections.
 func (cp *connectionPool) Conns() (uint32, uint32) {
-	conns := uint32(cp.conns.Load())
-	return conns - uint32(len(cp.connsChan)), conns
+	cp.connsMutex.Lock()
+	defer cp.connsMutex.Unlock()
+	if cp.closed {
+		return 0, 0
+	} else {
+		conns := cp.conns
+		return conns - uint32(len(cp.connsChan)), conns
+	}
+}
+
+// Returns number of max used connections.
+func (cp *connectionPool) MaxConns() uint32 {
+	cp.connsMutex.Lock()
+	defer cp.connsMutex.Unlock()
+	return cp.maxConnsUsed
 }
 
 // factory function for the new nntp connections
@@ -267,35 +291,33 @@ func (cp *connectionPool) factory() (*nntp.Conn, error) {
 }
 
 func (cp *connectionPool) addConn() {
-	cp.addConnChan <- connRequest{retryNo: 0, lastErr: nil}
+	cp.connsMutex.Lock()
+	defer cp.connsMutex.Unlock()
+	if !cp.closed {
+		cp.addConnChan <- connRequest{}
+	}
 }
 
-func (cp *connectionPool) closeConn(conn *NNTPConn) error {
-	cp.conns.Add(-1)
-	select {
-	case LogChan <- fmt.Sprintf("%v of %v connections available", cp.conns.Load(), cp.maxConns):
-	default:
-	}
-	return conn.close()
+func (cp *connectionPool) closeConn(conn *NNTPConn) {
+	cp.connsMutex.Lock()
+	defer cp.connsMutex.Unlock()
+	cp.conns--
+	cp.connAttempts--
+	cp.debug(fmt.Sprintf("%v of %v connections available", cp.conns, cp.maxConns))
+	conn.close()
 }
 
 func (cp *connectionPool) checkConnIsHealthy(conn NNTPConn) bool {
 	// closing expired connection
 	if cp.idleTimeout > 0 &&
 		conn.timestamp.Add(cp.idleTimeout).Before(time.Now()) {
-		select {
-		case LogChan <- "closing expired connection":
-		default:
-		}
+		cp.debug("closing expired connection")
 		cp.closeConn(&conn)
 		return false
 	}
 	// closing unhealthy connection
 	if err := conn.ping(); err != nil {
-		select {
-		case LogChan <- "closing unhealthy connection":
-		default:
-		}
+		cp.debug("closing unhealthy connection")
 		cp.closeConn(&conn)
 		return false
 	}
@@ -303,63 +325,94 @@ func (cp *connectionPool) checkConnIsHealthy(conn NNTPConn) bool {
 }
 
 func (cp *connectionPool) addConnHandler() {
-	for connRequest := range cp.addConnChan {
+	for range cp.addConnChan {
+		go func() {
+			cp.connsMutex.Lock()
+			if cp.connAttempts < cp.serverLimit {
+				cp.connAttempts++
+				cp.connsMutex.Unlock()
+				connStartTime := time.Now()
 
-		// check connection attempts
-		connAttempts := cp.connAttempts.Add(1)
-		if connAttempts > int32(cp.maxConns) || (cp.serverLimit != 0 && connAttempts > int32(cp.serverLimit)) {
-			// too many connection attempts
-			cp.connAttempts.Add(-1)
-			continue
-		}
+				// try to open the connection
+				conn, err := cp.factory()
 
-		// check if it is a retry
-		if connRequest.retryNo > 0 {
-			select {
-			case LogChan <- fmt.Sprintf("waiting %v seconds for next connection retry", cp.connWaitTime):
-			default:
-			}
-			time.Sleep(time.Until(connRequest.lastConnTime.Add(cp.connWaitTime)))
-		}
-
-		// try to open the connection
-		conn, err := cp.factory()
-
-		// connection error
-		if err != nil {
-			cp.connAttempts.Add(-1)
-			select {
-			case WarnChan <- err:
-			default:
-			}
-			if connRequest.retryNo > cp.connRetries {
-				if err.Error() == "482 too many connections for your user" && cp.conns.Load() > 0 {
-					select {
-					case LogChan <- fmt.Sprintf("reducing max connections setting to %v due to repeated 'too many connections' error", cp.conns.Load()):
-					default:
+				cp.connsMutex.Lock()
+				if !cp.closed {
+					if err != nil {
+						// connection error
+						cp.error(err)
+						cp.connAttempts--
+						if cp.maxTooManyConnsErrors > 0 && (err.Error()[0:3] == "482" || err.Error()[0:3] == "502") && cp.conns > 0 {
+							cp.tooManyConnsErrors++
+							if cp.tooManyConnsErrors > cp.maxTooManyConnsErrors && cp.serverLimit > cp.conns {
+								cp.serverLimit = cp.conns
+								cp.error(fmt.Errorf("reducing max connections to %v due to repeated 'too many connections' error", cp.serverLimit))
+							}
+						} else {
+							if cp.maxConnErrors > 0 {
+								cp.connErrors++
+								if cp.connErrors > cp.maxConnErrors && cp.conns == 0 {
+									cp.fatalError = fmt.Errorf("unable to establish a connection after %v retries  - last error was: %v", cp.connErrors-1, err)
+									cp.error(cp.fatalError)
+									cp.connsMutex.Unlock()
+									cp.Close()
+									return
+								}
+							}
+						}
+						go func() {
+							cp.debug(fmt.Sprintf("waiting %v seconds for next connection retry", cp.connWaitTime))
+							time.Sleep(cp.connWaitTime)
+							cp.addConnChan <- connRequest{}
+						}()
+					} else {
+						// connection successfull
+						cp.debug(fmt.Sprintf("connection took %v", time.Since(connStartTime)))
+						cp.tooManyConnsErrors = 0
+						cp.connErrors = 0
+						nntpConn := NNTPConn{
+							Conn:      conn,
+							timestamp: time.Now(),
+						}
+						select {
+						// push connection to the connections channel
+						case cp.connsChan <- nntpConn:
+							cp.conns++
+							cp.debug(fmt.Sprintf("new connection opened (%v of %v connections available)", cp.conns, cp.serverLimit))
+							// if the connections channel is full, close the connection
+						default:
+							cp.closeConn(&nntpConn)
+						}
 					}
-					cp.serverLimit = uint32(cp.conns.Load())
-				} else {
-					cp.fatalError = err
 				}
-			}
-		} else {
-			select {
-			case LogChan <- fmt.Sprintf("connection took %v", time.Since(cp.lastConnTime)):
-			default:
-			}
-			// connection successful
-			cp.connsChan <- NNTPConn{
-				Conn:      conn,
-				timestamp: time.Now(),
-			}
-			cp.conns.Add(1)
-			select {
-			case LogChan <- fmt.Sprintf("new connection opened (%v of %v connections available)", cp.conns.Load(), cp.maxConns):
-			default:
-			}
-		}
 
+			}
+			cp.connsMutex.Unlock()
+			if !cp.created {
+				cp.startupWG.Done()
+			}
+		}()
+	}
+}
+
+func (cp *connectionPool) log(text string) {
+	select {
+	case LogChan <- fmt.Sprintf("%s: %s", cp.name, text):
+	default:
+	}
+}
+
+func (cp *connectionPool) error(err error) {
+	select {
+	case WarnChan <- fmt.Errorf("%s: %v", cp.name, err):
+	default:
+	}
+}
+
+func (cp *connectionPool) debug(text string) {
+	select {
+	case DebugChan <- fmt.Sprintf("%s: %v", cp.name, text):
+	default:
 	}
 }
 
@@ -371,10 +424,13 @@ func (c *NNTPConn) close() error {
 }
 
 func (c *NNTPConn) ping() error {
-	if c.Conn != nil {
-		_, err := c.Conn.Date()
-		return err
-	} else {
-		return errConnIsNil
-	}
+	return nil
+	/*
+		if c.Conn != nil {
+			_, err := c.Conn.Date()
+			return err
+		} else {
+			return errConnIsNil
+		}
+	*/
 }
