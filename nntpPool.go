f73a341683895c5cc9c678cd1aad4f5a51bd0137
Tensai <tensai@gmx.net>
2024-06-01 10:57:43 +0200

    Fix missing Unlock in Put()

--- nntpPool.go
+++ nntpPool.go
@@ -102,8 +102,6 @@ type connectionPool struct {
 	maxConnsUsed       uint32
 }
 
-type connRequest struct{}
-
 // Opens new connection pool.
 func New(cfg *Config, initialConns uint32) (ConnectionPool, error) {
 	switch {
@@ -209,7 +207,7 @@ func (cp *connectionPool) Put(conn *NNTPConn) {
 	if !cp.closed {
 		select {
 		case cp.connsChan <- NNTPConn{Conn: conn.Conn, timestamp: time.Now()}:
-			cp.debug("connection successfully returned to the pool")
+			cp.connsMutex.Unlock()
 			return
 		default:
 			cp.debug("closing returned connection because pool is full")
